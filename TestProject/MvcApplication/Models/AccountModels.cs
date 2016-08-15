b639a97623ff6f1ba1bf0165a702845f5e406271
Lee <382117535@qq.com>
2016-08-15 15:53:09 +0800

    PacageUpdate

--- TestProject/MvcApplication/Models/AccountModels.cs
+++ TestProject/MvcApplication/Models/AccountModels.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
-using System.Globalization;
-using System.Web.Mvc;
-using System.Web.Security;
 
 namespace MvcApplication.Models
 {
