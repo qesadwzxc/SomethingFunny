f0ea41a9a38efb62a1378f524cfb2c7d6b226cf5
仲炎 <zy14246@ly.com>
2016-10-24 11:11:44 +0800

    = =

---
+++ TestProject/SummerShow/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SummerShow
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TestProject/SummerShow/Controllers/FormController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SummerShow.Controllers
+{
+    public class FormController : Controller
+    {
+        //
+        // GET: /Form/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
