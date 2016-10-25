b58ce3e63bf593336e7bcc3d9d53dc67c5a8067a
Lee <382117535@qq.com>
2016-10-25 14:50:21 +0800

    clean

--- TestProject/SummerShow/Global.asax.cs
+++ TestProject/SummerShow/Global.asax.cs
@@ -17,12 +17,10 @@ namespace SummerShow
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }
